3c9698d732b12ed7604b8283804d59e33dc30137
Hazim's Apprentice <benjaminlarin23@gmail.com>
2015-08-10 17:08:49 +0800

    Minimap
    Updated then added Ben's stuffs

---
+++ PGE Project - Top Down Shooter/Assets/2D Textures/UI/GUIManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GUIManager : MonoBehaviour {
+
+	public RenderTexture MinimapTex;
+	public Material MinimapMat;
+
+	public float minX;
+	public float maxX;
+	public float minY;
+	public float maxY;
+
+	void Awake () {
+	}
+
+	void OnGUI () {
+		if(Event.current.type == EventType.Repaint)
+			Graphics.DrawTexture(new Rect(minX, 
+			                              minY, 
+			                              maxX, 
+			                              maxY), 
+			                     MinimapTex, 
+			                     MinimapMat);
+	}
+}
